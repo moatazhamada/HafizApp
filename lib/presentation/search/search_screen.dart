6a09fb24899033c7c7b2ac0dc8f5f3cfc29c2816
Moataz Mohamed <motazhamada@gmail.com>
2026-01-12 13:46:50 +0200

    fix(lint): add bloc dependency and resolve deprecations

--- lib/presentation/search/search_screen.dart
+++ lib/presentation/search/search_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../core/app_export.dart';
 import '../../injection_container.dart';
 import 'bloc/search_bloc.dart';
